4e99741c9968fad3d56dd82355c38408fc3e4f02
mehmed <mehmed2kw@gmail.com>
2023-05-21 16:23:20 +0300

    edited header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,35 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.scss";
-import { Link } from 'react-router-dom';
+import { Link } from 'react-scroll';
 
 const Header = () => {
-  return (
-    <header className="header">
-        <div className="shell">
-            <div className="header__inner">
-                <nav className="header__nav">
-                    <ul>
-                        <li>
-                            <Link>Intro</Link>
-                        </li>
-                        
-                        <li>
-                            <Link>Projects</Link>
-                        </li>
+    const [changeColor, setChangeColor] = useState("");
+    window.addEventListener('scroll', () => {
+        let currentScrollPos = window.pageYOffset;
 
-                        <li>
-                            <Link>About Me</Link>
-                        </li>
+        if (currentScrollPos > 0) {
+            setChangeColor("scroll-color");
+        } else {
+            setChangeColor("");
+        }
+    });
 
-                        <li>
-                            <Link>Contact</Link>
-                        </li>
-                    </ul>
-                </nav>
+    return (
+        <header className={`header ${changeColor}`}>
+            <div className="shell">
+                <div className="header__inner">
+                    <nav className="header__nav">
+                        <ul>
+                            <li>
+                                <Link to="intro" smooth={true} duration={500}>Intro</Link>
+                            </li>
+
+                            <li>
+                                <Link to="projects" smooth={true} duration={500}>Projects</Link>
+                            </li>
+
+                            <li>
+                                <Link to="about" smooth={true} duration={500}>About Me</Link>
+                            </li>
+
+                            <li>
+                                <Link to="contact" smooth={true} duration={500}>Contact</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
             </div>
-        </div>
-    </header>
-  )
+        </header>
+    )
 }
 
 export default Header
