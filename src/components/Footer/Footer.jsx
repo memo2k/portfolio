0dd8c57b37d5247c991778b32b362bb1a7cb243c
mehmed <mehmed2kw@gmail.com>
2023-05-21 19:55:59 +0300

    added form function

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,64 +1,78 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import "./footer.scss";
-import { Link } from 'react-scroll';
+import { Link } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 
 const Footer = () => {
-  return (
-    <footer name='contact' className="footer">
-        <div className="shell">
-            <div className="footer__inner">
-                <div className="footer__form">
-                    <div className="form">
-                        <div className="form__subheading">
-                            <h2>Send me a message!</h2>
-                        </div>
+    const form = useRef();
 
-                        <div className="form__heading">
-                            <h1>Feel free to contact me by submitting the form below</h1>
-                        </div>
+    const sendEmail = (e) => {
+        e.preventDefault();
 
-                        <div className="form__body">
-                            <div className="form__row">
-                                <div className="form__field">
-                                    <input type="text" placeholder='Name' id="name" />
+        emailjs.sendForm('service_tccldcb', 'template_wkmfo22', form.current, 'Tf_taS-SPX-Y4yESa')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+
+        e.target.reset();
+    }
+    return (
+        <footer name='contact' className="footer">
+            <div className="shell">
+                <div className="footer__inner">
+                    <div className="footer__form">
+                        <form className="form" onSubmit={sendEmail} ref={form}>
+                            <div className="form__subheading">
+                                <h2>Send me a message!</h2>
+                            </div>
+
+                            <div className="form__heading">
+                                <h1>Feel free to contact me by submitting the form below</h1>
+                            </div>
+
+                            <div className="form__body">
+                                <div className="form__row">
+                                    <div className="form__field">
+                                        <input name="user_name" type="text" placeholder='Name' id="name" required />
+                                    </div>
+
+                                    <div className="form__field">
+                                        <input name="user_email" type="email" placeholder='Email' id='email' required />
+                                    </div>
                                 </div>
 
-                                <div className="form__field">
-                                    <input type="email" placeholder='Email' id='email' />
+                                <div className="form__row">
+                                    <div className="form__field">
+                                        <textarea name="message" id="" cols="100" rows="10" placeholder='Enter your message' required ></textarea>
+                                    </div>
                                 </div>
                             </div>
 
-                            <div className="form__row">
-                                <div className="form__field">
-                                    <textarea name="" id="" cols="100" rows="10" placeholder='Enter your message'></textarea>
-                                </div>
+                            <div className="form__actions">
+                                <button className="btn btn--transparent">Send</button>
                             </div>
-                        </div>
-
-                        <div className="form__actions">
-                            <Link className="btn btn--red">Send</Link>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="footer__nav">
-                    <div className="footer__copyright">
-                        <p>&copy; Mehmed dev 2023</p>
+                        </form>
                     </div>
 
-                    <div className="footer__socials">
-                        <nav>
-                            <ul>
-                                <Link to="/"><i class="fa-brands fa-github fa-xl"></i></Link>
-                                <Link to="/"><i class="fa-brands fa-linkedin fa-xl"></i></Link>
-                            </ul>
-                        </nav>
+                    <div className="footer__nav">
+                        <div className="footer__copyright">
+                            <p>&copy; Mehmed dev 2023</p>
+                        </div>
+
+                        <div className="footer__socials">
+                            <nav>
+                                <ul>
+                                    <Link to="https://github.com/memo2k" target='_blank'><i class="fa-brands fa-github fa-xl"></i></Link>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </footer>
-  )
+        </footer>
+    )
 }
 
 export default Footer
