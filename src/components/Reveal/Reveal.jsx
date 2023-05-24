fdb8118fc01d91e02c3e0f7bf5cc270310675df9
mehmed <mehmed2kw@gmail.com>
2023-05-24 12:33:33 +0300

    created Reveal component

---
+++ src/components/Reveal/Reveal.jsx
@@ -0,0 +1,22 @@
+import React, { useRef } from 'react';
+import { useInView } from "framer-motion";
+
+const Reveal = ({ children }) => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
+  return (
+    <div
+      ref={ref}
+      style={{
+        transform: isInView ? "none" : "translateY(-100px)",
+        opacity: isInView ? 1 : 0,
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+      }}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default Reveal
