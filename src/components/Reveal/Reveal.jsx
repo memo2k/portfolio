eea5ac6d5a74f2962c47e367c233e54149048d94
mehmed <mehmed2kw@gmail.com>
2023-05-24 17:37:02 +0300

    updated component

--- src/components/Reveal/Reveal.jsx
+++ src/components/Reveal/Reveal.jsx
@@ -9,7 +9,7 @@ const Reveal = ({ children }) => {
     <div
       ref={ref}
       style={{
-        transform: isInView ? "none" : "translateY(-100px)",
+        transform: isInView ? "none" : "translateX(-200px)",
         opacity: isInView ? 1 : 0,
         transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
       }}
